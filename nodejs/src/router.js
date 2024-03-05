881cb70a24ef3cac672dd9104dc2fd7ce3486f36
yyfx <118585028+yydfys@users.noreply.github.com>
2024-03-05 14:39:41 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -6,7 +6,8 @@ import ffm3u8 from './spider/video/ffm3u8.js';
 import push from './spider/video/push.js';
 import alist from './spider/pan/alist.js';
 import _13bqg from './spider/book/13bqg.js';
-const spiders = [douban,wogg,kunyu77, kkys, ffm3u8, push, alist, _13bqg];
+import copymanga from './spider/book/copymanga.js';
+const spiders = [douban,wogg,kunyu77, kkys, ffm3u8, push, alist, _13bqg,copymanga];
 const spiderPrefix = '/spider';
 
 /**
