8e04ed2b13d2bdb69e7227001f04ba0d830cc076
yyfx <118585028+yydfys@users.noreply.github.com>
2024-03-06 12:08:30 +0800

    Add files via upload

--- nodejs/src/spider/video/lzm3u8.js
+++ nodejs/src/spider/video/lzm3u8.js
@@ -12,8 +12,8 @@ async function request(reqUrl) {
 }
 
 async function init(inReq, _outResp) {
-    url = inReq.server.config.ffm3u8.url;
-    categories = inReq.server.config.ffm3u8.categories;
+    url = inReq.server.config.lzm3u8.url;
+    categories = inReq.server.config.lzm3u8.categories;
     return {};
 }
 
