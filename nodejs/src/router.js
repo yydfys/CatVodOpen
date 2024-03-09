06d96c1fa813912a766375e0326a752d4c77dc91
yyfx <118585028+yydfys@users.noreply.github.com>
2024-03-10 07:13:15 +0800

    Add files via upload

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -31,7 +31,7 @@ import kuaikan from './spider/video/kuaikan.js';
 import wjm3u8 from './spider/video/wjm3u8.js';
 import maiyoux from './spider/video/maiyoux.js';
 
-const spiders = [douban, wogg, ysche, wobg, tudou, baipiaoys, kunyu77, kkys, nangua, wenku, bg, ffm3u8, hhm3u8, lzm3u8, subm3u8, hnm3u8, xlm3u8, kuaikan, wjm3u8, maiyoux, fengche, xiaoya, xxpan, cntv, push, alist, _13bqg, goda, laobaigs, baozimh, copymanga];
+const spiders = [douban, wogg, ysche, wobg, tudou, baipiaoys, kunyu77, kkys, nangua, wenku, bg, ffm3u8, hhm3u8, lzm3u8, subm3u8, hnm3u8, xlm3u8, kuaikan, wjm3u8, fengche, xiaoya, xxpan, cntv, push, alist, _13bqg, goda, laobaigs, baozimh, copymanga, maiyoux];
 const spiderPrefix = '/spider';
 
 /**
