b27242890663de1f9cb84746a7e47638a16b4b3f
yyfx <118585028+yydfys@users.noreply.github.com>
2024-03-12 20:06:12 +0800

    Add files via upload

--- nodejs/src/spider/video/upyun.js
+++ nodejs/src/spider/video/upyun.js
@@ -1,41 +1,117 @@
 import req from '../../util/req.js';
+import pkg from 'lodash';
+const { _ } = pkg;
 import { MAC_UA, formatPlayUrl } from '../../util/misc.js';
 import { load } from 'cheerio';
 import * as HLS from 'hls-parser';
-import { getDownload, getFilesByShareUrl, getLiveTranscoding, getShareData, initAli } from '../../util/ali.js';
+import * as Ali from '../../util/ali.js';
+import { getDownload, getFilesByShareUrl, getLiveTranscoding, getShareData, initAli} from '../../util/ali.js';
 import dayjs from 'dayjs';
 import CryptoJS from 'crypto-js';
+
+let patternAli = /(https:\/\/www\.(aliyundrive|alipan)\.com\/s\/[^"]+)/
 
 let url = 'https://zyb.upyunso.com';
 
 async function request(reqUrl) {
-    let res = await req(reqUrl, {
-        method: 'get',
+    let res = await req.get(reqUrl, {
         headers: {
             'Referer': url,
         },
     });
-    return res.content;
+    return res.data;
 }
+
 
 async function init(inReq, _outResp) {
     await initAli(inReq.server.db, inReq.server.config.ali);
-    return {};
+    return {}
 }
 
-async function home(_inReq, _outResp) {}
 
-async function category(inReq, _outResp) {}
+async function home(inReq,_outResp){
+    return {}
+}
+
+
+async function category(inReq, _outResp) {
+    return{}
+}
 
 async function detail(inReq, _outResp) {
-    return await detailContent(id);
+    const ids = !Array.isArray(inReq.body.id) ? [inReq.body.id] : inReq.body.id;
+    const videos = [];
+    const shareUrls = ids;
+        let vod = ({
+            vod_id: shareUrls,
+        });
+        const froms = [];
+        const urls = [];
+        for (const shareUrl of shareUrls) {
+            const shareData = Ali.getShareData(shareUrl);
+            if (shareData) {
+                const videos = await Ali.getFilesByShareUrl(shareData);
+                if (videos.length > 0) {
+                    froms.push('Ali-' + shareData.shareId);
+                    urls.push(
+                        videos
+                            .map((v) => {
+                                const ids = [v.share_id, v.file_id, v.subtitle ? v.subtitle.file_id : ''];
+                                return formatPlayUrl('', v.name) + '$' + ids.join('*');
+                            })
+                            .join('#'),
+                    );
+                }
+            }
+        }
+        vod.vod_play_from = froms.join('$$$');
+        vod.vod_play_url = urls.join('$$$');
+        videos.push(vod);
+    return {
+        list: videos,
+    };
 }
+
+
+
+
+async function parseHtmlList(pg, wd) {
+    const limit = 25;
+    const resp = await request("https://zyb.upyunso.com/v15/search?keyword=" + encodeURIComponent(wd) + '&page=' + pg + '&s_type=2');
+    const data = decrypt(resp);
+    const items = JSON.parse(data).result.items;
+    const videos = [];
+    for(const item of items) {
+        const url = decrypt(item.page_url);
+        const matches = url.match(patternAli);
+        if (_.isEmpty(matches)) continue;
+        const title = _.isEmpty(item.content) ? item.title : item.content[0].title;
+        videos.push({
+            vod_id: url,
+            vod_name: title.replaceAll(/<\/?[^>]+>/g, ""),
+            vod_pic: "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000",
+            vod_remarks: item.insert_time,
+        });
+    }
+    const hasMore = !_.isEmpty(items);
+    const pgCount = hasMore ? parseInt(pg) + 1 : parseInt(pg);
+    return({
+        page: parseInt(pg),
+        pagecount: pgCount,
+        limit: limit,
+        total: limit * pgCount,
+        list: videos,
+    });
+}
+
+
+
 
 const transcodingCache = {};
 const downloadingCache = {};
 
 async function proxy(inReq, outResp) {
-    await initAli(inReq.server.db, inReq.server.config.ali);
+    await Ali.initAli(inReq.server.db, inReq.server.config.ali);
     const what = inReq.params.what;
     const shareId = inReq.params.shareId;
     const fileId = inReq.params.fileId;
@@ -61,7 +137,7 @@ async function proxy(inReq, outResp) {
         }
 
         if (!transcodingCache[fileId]) {
-            const transcoding = await getLiveTranscoding(shareId, fileId);
+            const transcoding = await Ali.getLiveTranscoding(shareId, fileId);
             transcodingCache[fileId] = transcoding;
         }
 
@@ -95,6 +171,7 @@ async function proxy(inReq, outResp) {
             return hls;
         }
     } else {
+        const flag = inReq.params.flag;
         if (downloadingCache[fileId]) {
             const purl = downloadingCache[fileId].url;
             if (parseInt(purl.match(/x-oss-expires=(\d+)/)[1]) - dayjs().unix() < 15) {
@@ -102,7 +179,7 @@ async function proxy(inReq, outResp) {
             }
         }
         if (!downloadingCache[fileId]) {
-            const down = await getDownload(shareId, fileId);
+            const down = await Ali.getDownload(shareId, fileId, flag == 'down');
             downloadingCache[fileId] = down;
         }
         outResp.redirect(downloadingCache[fileId].url);
@@ -113,7 +190,7 @@ async function proxy(inReq, outResp) {
 async function play(inReq, _outResp) {
     const id = inReq.body.id;
     const ids = id.split('*');
-    const transcoding = await getLiveTranscoding(ids[0], ids[1]);
+    const transcoding = await Ali.getLiveTranscoding(ids[0], ids[1]);
     transcoding.sort((a, b) => b.template_width - a.template_width);
     const urls = [];
     const proxyUrl = inReq.server.address().url + inReq.server.prefix + '/proxy';
@@ -122,69 +199,47 @@ async function play(inReq, _outResp) {
         urls.push(`${proxyUrl}/trans/${t.template_id.toLowerCase()}/${ids[0]}/${ids[1]}/.m3u8`);
     });
     urls.push('SRC');
-    urls.push(`${proxyUrl}/src/nil/${ids[0]}/${ids[1]}/.bin`);
+    urls.push(`${proxyUrl}/src/down/${ids[0]}/${ids[1]}/.bin`);
     const result = {
         parse: 0,
         url: urls,
     };
     if (ids[2]) {
         result.extra = {
-            subt: `${proxyUrl}/src/nil/${ids[0]}/${ids[2]}/.bin`,
+            subt: `${proxyUrl}/src/subt/${ids[0]}/${ids[2]}/.bin`,
         };
     }
     return result;
 }
 
+
+
+
 async function search(inReq, _outResp) {
-    const page = inReq.body.page;
+    let pg = inReq.body.page;
     const wd = inReq.body.wd;
-    let pg = page || 1;
     if (pg <= 0) pg = 1;
-    const limit = 25;
-    const resp = await request(url + "/v15/search?keyword=" + encodeURIComponent(wd) + '&page=' + pg + '&s_type=2');
-    const data = decrypt(resp);
-    const items = JSON.parse(data).result.items;
-    const videos = [];
-    for(const item of items) {
-        const url = decrypt(item.page_url);
-        const matches = url.match(patternAli);
-        if (_.isEmpty(matches)) continue;
-        const title = _.isEmpty(item.content) ? item.title : item.content[0].title;
-        videos.push({
-            vod_id: url,
-            vod_name: title.replaceAll(/<\/?[^>]+>/g, ""),
-            vod_pic: "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000",
-            vod_remarks: item.insert_time,
-        });
-    }
-    const hasMore = !_.isEmpty(items);
-    const pgCount = hasMore ? parseInt(pg) + 1 : parseInt(pg);
-    return JSON.stringify({
-        page: parseInt(pg),
-        pagecount: pgCount,
-        limit: limit,
-        total: limit * pgCount,
-        list: videos,
-        tline: 4
-    });
+    return parseHtmlList(pg,wd);
 }
+
 
 function decrypt(text) {
     const data = {
-        ciphertext: Crypto.enc.Hex.parse(text.toUpperCase()),
+        ciphertext: CryptoJS.enc.Hex.parse(text.toUpperCase()),
     };
-    const key = Crypto.enc.Utf8.parse('qq1920520460qqzz');
-    const iv = Crypto.enc.Utf8.parse('qq1920520460qqzz');
-    const mode = Crypto.mode.CBC;
-    const padding = Crypto.pad.Pkcs7;
-    const decrypted = Crypto.AES.decrypt(data, key, {
+    const key = CryptoJS.enc.Utf8.parse('qq1920520460qqzz');
+    const iv = CryptoJS.enc.Utf8.parse('qq1920520460qqzz');
+    const mode = CryptoJS.mode.CBC;
+    const padding = CryptoJS.pad.Pkcs7;
+    const decrypted = CryptoJS.AES.decrypt(data, key, {
         'iv': iv,
         'mode': mode,
         'padding': padding
     });
-    const decryptedData = Crypto.enc.Utf8.stringify(decrypted);
+    const decryptedData = CryptoJS.enc.Utf8.stringify(decrypted);
     return decryptedData;
 }
+
 
 
 async function test(inReq, outResp) {
@@ -202,7 +257,7 @@ async function test(inReq, outResp) {
         resp = await inReq.server.inject().post(`${prefix}/home`);
         dataResult.home = resp.json();
         printErr(resp.json());
-        if (dataResult.home.class.length > 0) {
+        if (dataResult.home.class && dataResult.home.class.length > 0) {
             resp = await inReq.server.inject().post(`${prefix}/category`).payload({
                 id: dataResult.home.class[0].type_id,
                 page: 1,
@@ -211,7 +266,7 @@ async function test(inReq, outResp) {
             });
             dataResult.category = resp.json();
             printErr(resp.json());
-            if (dataResult.category.list.length > 0) {
+            if (dataResult.category.list &&dataResult.category.list.length > 0) {
                 resp = await inReq.server.inject().post(`${prefix}/detail`).payload({
                     id: dataResult.category.list[0].vod_id, // dataResult.category.list.map((v) => v.vod_id),
                 });
@@ -257,8 +312,9 @@ async function test(inReq, outResp) {
 export default {
     meta: {
         key: 'upyun',
-        name: '🍀 up云搜',
+        name: '🍀 up云搜(仅搜索)',
         type: 3,
+        indexs: 1,
     },
     api: async (fastify) => {
         fastify.post('/init', init);
